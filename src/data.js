08ab3a9a89eaae5bb40a1cce7166f0539e26eda5
Karina Mel <92555432+KarinaHachimitsu@users.noreply.github.com>
2022-02-10 14:34:18 -0300

    refactor(JavaScript)Change names

--- src/data.js
+++ src/data.js
@@ -1,14 +1,3 @@
-import dadosGhibli from "./data/ghibli/ghibli.js";
-export function qualquercoisa() {
-  dadosGhibli.films.map((item) => {
-    let lista = document.createElement("ul");
-    let title = document.createElement("h1");
-    let poster = document.createElement("img");
-    lista.classList.add("cards");
-    lista.appendChild(poster);
-    lista.appendChild(title);
-    document.body.appendChild(lista);
-    poster.src = item.poster;
-    title.innerHTML = item.title;
-  });
-}
+import dataGhibli from "./data/ghibli/ghibli.js";
+console.log(dataGhibli);
+export function importToMain() {}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import dadosGhibli from "./data/ghibli/ghibli.js";
-import { qualquercoisa } from "./data.js";
-console.log(dadosGhibli);
-qualquercoisa();
+import dataGhibli from "./data/ghibli/ghibli.js";
+import { importToMain } from "./data.js";
+console.log(dataGhibli);
+importToMain();
+
+dataGhibli.films.map((items) => {
+  let card = document.createElement("ul");
+  let title = document.createElement("h1");
+  let poster = document.createElement("img");
+  card.classList.add("cards");
+  card.appendChild(poster);
+  card.appendChild(title);
+  document.body.appendChild(card);
+  poster.src = items.poster;
+  title.innerHTML = items.title;
+});

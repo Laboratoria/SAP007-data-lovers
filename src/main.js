8da466a03917fa09903320b0c2e109d718011c94
lud-ic <mag.ludmila@gmail.com>
2022-02-13 00:00:57 -0300

    feature[navBar]: added header/nav bar with responsive screen

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { example } from "./data.js";
 
-
 import data from "./data/ghibli/ghibli.js";
 
-
 console.log(example, data);
+
+let toggleButton = document.getElementById("toggleButton");
+let navList = document.getElementById("navList");
+
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});

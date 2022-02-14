135e5445f9e6f2d8f855da21c75bc828d4d03a09
Voskrov <monicapeixoto0@gmail.com>
2022-02-14 20:03:02 -0300

    grid nos cards

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-let namePokemon = document.getElementById('name-pokemon')
-let image = document.getElementById('img-pokemon')
-let numPokemon = document.getElementById('num-pokemon')
-let typePokemon = document.getElementById('type-pokemon')
-let weaknessesPokemon = document.getElementById('weaknesses-pokemon')
+const pokemons = data.pokemon
+// let namePokemon = document.getElementById('name-pokemon')
+// let image = document.getElementById('img-pokemon')
+// let numPokemon = document.getElementById('num-pokemon')
+// let typePokemon = document.getElementById('type-pokemon')
+// let weaknessesPokemon = document.getElementById('weaknesses-pokemon')
 
 
 
-console.log(example, data);
+
+const nameApear = () => {
+    for (let onePokemon of pokemons) {
+        let name = onePokemon.name
+        console.log(name)
+    }
+}
+nameApear()

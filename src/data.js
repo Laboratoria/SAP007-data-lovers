34ac78b54848be78b6c53376abf2ddf1ec6bac91
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-02-14 20:36:22 -0300

    restauração de dados

--- src/data.js
+++ src/data.js
@@ -8,3 +8,12 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
+// Mostrar quantidade de Pokémons que quisermos selecionar
+export const pokemonsData = (pokemonsCont) => pokemonsCont.slice(0, 251);
+
+export const filterByName = (pokemonsCont, pokemonsName) => {
+  const lowerCaseName = pokemonsName.toLowerCase();
+  return pokemonsList
+    .filter((pokemon) => pokemon.name.toLowerCase().startsWith(lowerCaseName))
+    .slice(0, 251);
+};

f4b0ca88a3830e1d603f2476f0bb647426c4c328
Voskrov <monicapeixoto0@gmail.com>
2022-02-15 16:48:52 -0300

    small card

--- src/main.js
+++ src/main.js
@@ -5,15 +5,20 @@ const pokemon = data.pokemon;
 
 let atribute = element.getAttribute 
 
-/*let namePokemon = document.getElementById('name-pokemon')
+let namePokemon = document.getElementById('name-pokemon').value
 let image = document.getElementById('img-pokemon')
 let numPokemon = document.getElementById('num-pokemon')
 let typePokemon = document.getElementById('type-pokemon')
-let weaknessesPokemon = document.getElementById('weaknesses-pokemon')*/
+let weaknessesPokemon = document.getElementById('weaknesses-pokemon')
 
-/*const pokemon = data.pokemon;
-
-
+const smallCardPokemon = () => {
+    let i = 0
+    for (let onePokemon of pokemon)
+        if (onePokemon[0]){
+            image.insertAdjacentHTML(`<img src="${onePokemon.img}" alt="Imagem Pokemon" class="">`)
+            namePokemon.insertAdjacentHTML(onePokemon.name)   
+        }
+}
 
 const pokemonFilter = () => {
     for (let pokemonData of data.pokemon) {

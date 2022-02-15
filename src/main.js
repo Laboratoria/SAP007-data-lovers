d22db5fa913bb1c87da7368783330e756d12a46f
Karol Sontak <karolsontak@gmail.com>
2022-02-15 16:09:22 -0300

    Alterações de CSS e início de episodio.html e personagens.html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { funções criadas no data.js } from './data.js';
+//import {filtroGenero, filtroEspecie, filtroStatus, ordemAlfabetica, buscarNome, calcularPorcentagem} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+//////////////////FUNÇÃO DE IMPRIMIR AS INFORMAÇÕES NA TELA
+function infoCardsTela (data) {
+  document.getElementById("info-card").innerHTML = data.map((item) => `
+  <li class="cards">
+  <img class="card-img"> src="${item.image}"</img>
+    <h2 class="card-title"> src="${item.name}"</h2>
+  </li>
+`).join('')
+}
+infoCardsTela(data.results);
 
 
 
@@ -20,9 +30,3 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 
 
-
-
-
-
-
-//console.log(example, data);

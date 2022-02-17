011a68ff68ced34ae9833f3fe779f310ca0ad39e
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-02-17 18:10:43 -0300

    Testando

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+
+/*
+Recomendamos que utilize src/main.js 
+para todos os códigos que tenham a ver com a exibição dos dados na tela. 
+Com isto nos referimos basicamente à interação com o DOM. Operações como criação de nós, 
+registro de manejadores de eventos (event listeners ou event handlers) e etc.
+
+Neste arquivo você encontrará uma série de imports comentados. 
+Para carregar diferentes fontes de dados, você deverá "descomentar" esses imports.
+ Cada um destes imports criará uma variável DATA com os dados correspondentes à fonte escolhida.
+*/
+
 import data from './data/rickandmorty/rickandmorty.js';
 
 function mostrarCards(data) {

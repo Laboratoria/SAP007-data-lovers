12a9aec91c2d11ab70f53b2efa063e6945ec84f0
Karol Sontak <karolsontak@gmail.com>
2022-02-17 15:37:47 -0300

    Cards aparecendo na tela, problema com SELECT no HTML - com ele, os cards param de aparecer

--- src/data.js
+++
@@ -1,8 +0,0 @@
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
--- src/main.js
+++ src/main.js
@@ -1,28 +1,16 @@
-import { funções criadas no data.js } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+function mostrarCards(data) {
+    document.getElementById('recebe-card').innerHTML = data.map((item) => `
+  <div class="card">
+    <div class="card-interno">
+        <div class="card-frente">
+         <img class="imagem-card" src="${item.image}">
+         <p><b> ${item.name}</b></p>
+        </div> 
+    </div>
+  </div>  
+`)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//console.log(example, data);
+mostrarCards(data.results);

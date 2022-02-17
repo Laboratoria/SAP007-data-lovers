3002af8ad120523205d69ff0e10b6263ae11e5f0
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-02-17 14:20:20 -0300

    Changes RIck and Morty

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/ghibli/ghibli.js';
-// import data from './data/athletes/athletes.js';
+
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);

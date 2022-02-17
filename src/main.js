832f286c08bb0e743c89e86975d4424b71d595a3
BeaSCarvalho <beaproscarva@gmail.com>
2022-02-17 19:10:06 -0300

    feat:js pegando infos do data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+function showResults(event){
+    event.preventDefault();
+
+    for(let i = 0; i < data.pokemon.length; i++){
+       let allDatas = data.pokemon[i];
+       let onlyNecessaryInfos = allDatas.num + allDatas.name + allDatas.type + allDatas.weaknesses + allDatas.img;
+       let pokemonCard = document.createElement("div");
+       pokemonCard.innerText = onlyNecessaryInfos;
+       document.body.appendChild(pokemonCard);
+       pokemonCard.classList.add(".style-para-o-elemento-recem-criado");
+       console.log(typeof(onlyNecessaryInfos));
+    }   
+}   
+
+document.getElementById("confirm-button").addEventListener("click",showResults);
+
+
+    /*const pokemonNumber = allDatas.num;
+    console.log(pokemonNumber);
+    const pokemonName = allDatas.name;
+    console.log(pokemonName);
+    const pokemonType = allDatas.type;
+    console.log(pokemonType);
+    const pokemonWeaknesses = allDatas.weaknesses;
+    console.log(pokemonWeaknesses);*/
+
 

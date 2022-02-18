0edf364badcecc2127121e06dcb8c605c0b0fe78
Taize Santos <taizeonze@gmail.com>
2022-02-18 06:50:52 -0300

    atualização dos arqvuivos

--- src/data.js
+++ src/data.js
@@ -1,25 +1,10 @@
-/*export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};*/
-
-//funcao filtar filmes e retorna dados dos filmes
-export const filterData = (data ) => {                                          //exporta a funcao; recebe os dados
-
-    //cria array pra guardar os filmes
-    let movies = [];                                                            //cria array pra filmes
-    
-    //acessa dados, entra em filmes, percorre cada um
-    data["films"].forEach(movie => {                                            //acessa os filmes dos dados; percorre cada um
+export const filterData = (data ) => {                                          
+    let movies = [];                                                            
+    data["films"].forEach(movie => {                                            
 
         //manda pro array de movies o conteudo dos filmes
-       movies.push(                                                                // manda pro let de movies o conteudo abaixo 
-           {               
-               //propriedade q vai usar no main: parameto[valor nome = do data]                                                             
-               "title": movie["title"],                                             // propriedade: parameto  e o valor [nome = a q ta no data]
+       movies.push(                                                                                                                      {
+               "title": movie["title"],                                      
                "poster": movie["poster"],
                "year": movie["release_date"],
                "score": movie["rt_score"],
@@ -33,27 +18,14 @@ export const filterData = (data ) => {                                          
     return movies;
 };
 
-//testeFiltrobranch
 
 //funcao que filtra e retorna dados de personagens
-export const filterCharacter = (data) =>{                                   // exporta a funcao; recebe dados
-
-    //cria array pra guardar personagens
-    let people = [];                                                          // cria array de pessoas
-
-    //dentro dos dados; entra em filmes e percorre cada um
-    data["films"].forEach(function(film) {                                      //entra obj film; percorre com foreach             
-       // console.log(filme["title"]);       
-       
-       //dentro de filmes; entra em pessoas e percorre cada um 
-        film["people"].forEach(function(person){                                     // dentro do obj filme percorre e tras o obj people          
-           // console.log(people["name"]);
-
-           //manda pro array de people o conteudo de pessoas
-            people.push(                                                            //mando pro array de pessoas o conteudo abaixo:
+export const filterCharacter = (data) =>{                                  
+    let people = [];                                                         
+    data["films"].forEach(function(film) {                                      
+        film["people"].forEach(function(person){                                     
+            people.push(                                                           
                 {
-
-                    //propriedade q vai usar no personagens js: parametro[valor da propridade, nome igual o q esta no data]
                     "name": person["name"],
                     "age": person["age"],                                               
                     "gender": person["gender"],
@@ -67,45 +39,17 @@ export const filterCharacter = (data) =>{                                   // e
     return people;
 }
 
-export const ordenar = (data, condicao) => {
+// export const ordenar = (data, condicao) => {
 
-    let ordenaTituloAZ = [data["title"]]
+//     let ordenaTituloAZ = [data["title"]]
 
-   let teste = ordenaTituloAZ.sort((a,b) =>{
-       return a.title.localeCompare(b.title);
+//    let teste = ordenaTituloAZ.sort((a,b) =>{
+//        return a.title.localeCompare(b.title);
        
-   });
+//    });
    
-console.log(teste);
+// console.log(teste);
     
     
-export function ordenar = (data) => {
- const ordenarAnoAsc = (a,b)
-
-
-
-
-//sortData(data, sortBy, sortOrder)
-//computeStats(data)
-
-/*export const ordenar = (data) => {
-
-    let ordenaTituloAZ = [data["title"]]
-
-    ordenaTituloAZ.sort(function(a, b){
-        let ta = a.title,
-            tb = b.title;
-
-        if (ta < tb) {
-            return -1;
-        }
-        if (ta > tb) {
-            return 1;
-        }
-        return 
-        
-
-    })
-    ordenaTituloAZ.push
-    console.log(ordenaTituloAZ)
-};*/
+// export function ordenar = (data) => {
+//  const ordenarAnoAsc = (a,b)
--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import { filterData } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-/*const recebeData = data;                                                        //salva dados
-recebeData["films"].forEach(function(filme) {                                      //entra obj film; percorre com foreach             
-    console.log(filme["title"]);                                                  // dentro do obj films tras o title  propriedade  
-    filme["people"].forEach(function(people){                                     // dentro do obj filme percorre e tras o obj people          
-        console.log(people["name"]);                                              // dentro do obj peolple tras propriedade name          
-    })
-});
 
-recebeData["films"][1]["title"];
-console.log(recebeData["films"][1]["title"]);*/
-
-const filtro = document.getElementById("selecao");
+const filtro = document.getElementById("selecionaFiltro");
 filtro.addEventListener("change", function()  {
     const valor = filtro.value;
     console.log(valor)
@@ -28,12 +18,11 @@ filtro.addEventListener("change", function()  {
          escolhido.push
          break;
      }
-     //escolhido = document.forms[0].selecao.value;
+     //escolhido = document.forms[0].selecionaFiltro.value;
      //return escolhido;
      console.log(escolhido)
  }
  
-    
 
 //funcao que manda pro filtro de filme os dados e recebe o array com todos os filmes
 function getMovies () {      
@@ -49,63 +38,28 @@ function getMovies () {
 console.log(getMovies());
 
 
+let card = document.getElementById("listaFilmes");
+let items = getMovies(); 
+let li;                                                                                
 
-//pega elemento pelo id da lista e guarda no card
-let card = document.getElementById("listaFilmes");                                     // pega id lista
 
-//guarda no items o array de filmes
-let items = getMovies();                                                                // guarda o array de obj(atributos) dos filmes
-let li;                                                                                  // var de item de lista
-
-//percorre cada item do array ; 
-items.forEach(function(movie){                                                                      //percorre cada item 
-
-    //cria item na lista
-    li = document.createElement("li");                                                                 //cria elemento na lista 
-
-    //add conteudo no item criado
-    li.appendChild(document.createTextNode(movie["title"] + "(" + movie["year"] + "). " ));            // add no novo li com conteudo                 
+items.forEach(function(movie){ 
+    li = document.createElement("li");
+    li.appendChild(document.createTextNode(movie["title"] + "(" + movie["year"] + "). " ));                 
     li.appendChild(document.createTextNode(" Nota de avaliaçāo: "+ movie["score"] + "."));
     li.appendChild(document.createTextNode(" Direçāo: " + movie["director"] + ". Produção: " + movie["producer"]+ "."));
-
-    //add img no li; passa pra funcao de img os parametos de src e alt
-    li.appendChild(displayImage(movie["poster"],movie["title"]));                                           //add no novo li a img, chama funcao da img e passa parametros
-
-    //manda pra variavel da lista os itens criados
-    card.appendChild(li);                                                                                   //add li criado na lista   
+    li.appendChild(displayImage(movie["poster"],movie["title"]));                                  
+    card.appendChild(li);         
 
 });
 
 
 //funca de mostrar imagem
-function displayImage(src, /*width, height*/  alt) {                                                        //pega os parametos da imagem, fonte e alt
-    let picture = document.createElement("img");                                                            // cria elemento de img        
+function displayImage(src, /*width, height*/  alt) {                                                
+    let picture = document.createElement("img");                                               
     picture.src = src;
-    
     //picture.style.width= "250px";
     /*picture.style.height = "250px";*/
-    
     picture.alt = alt;
     return picture;
 }
-
-
-
-
-
-
-
-//const recebeTitulo = films.find(films.title);
-//console.log(recebeTitulo);
-
-
-/*let dados = data.films
-
- const recebeData = data.films.filter(title);
-console.log(recebeData);
-
-//console.log(example, data);*/
-
-
-
-// \\TESSSTE

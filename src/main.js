9971a55c4279ee5b8bce7c8a6a322bcfde4b1e26
BeaSCarvalho <beaproscarva@gmail.com>
2022-02-18 17:27:03 -0300

    feat/ajuste-html

--- src/main.js
+++ src/main.js
@@ -9,6 +9,19 @@ function showResults(event){
     for(let i = 0; i < data.pokemon.length; i++){
        let allDatas = data.pokemon[i];
        
+       <section class="show-the-cards">
+      <div class="img-box">
+        <img src="naosei.img" alt=""></img>
+      </div>
+         
+      <div class="text-box">
+        <p class="poke-name">Nome</p>
+        <p class="poke-number">N°</p>
+        <p class="poke-type">Tipo</p>
+        <p class="poke-weaknesses">Fraqueza</p>
+      </div>
+    </section>
+       
        allDatas.num
        allDatas.name
        allDatas.type

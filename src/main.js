dae203b1b3f4edd952e6bc764456e7d136753480
BeaSCarvalho <beaproscarva@gmail.com>
2022-02-18 16:34:45 -0300

    feat/section-card

--- src/main.js
+++ src/main.js
@@ -8,25 +8,18 @@ function showResults(event){
 
     for(let i = 0; i < data.pokemon.length; i++){
        let allDatas = data.pokemon[i];
-       let onlyNecessaryInfos = allDatas.num + allDatas.name + allDatas.type + allDatas.weaknesses + allDatas.img;
-       let pokemonCard = document.createElement("div");
-       pokemonCard.innerText = onlyNecessaryInfos;
-       document.body.appendChild(pokemonCard);
-       pokemonCard.classList.add(".style-para-o-elemento-recem-criado");
-       console.log(typeof(onlyNecessaryInfos));
+       
+       allDatas.num
+       allDatas.name
+       allDatas.type
+       allDatas.weaknesses
+       allDatas.img
     }   
 }   
 
 document.getElementById("confirm-button").addEventListener("click",showResults);
 
 
-    /*const pokemonNumber = allDatas.num;
-    console.log(pokemonNumber);
-    const pokemonName = allDatas.name;
-    console.log(pokemonName);
-    const pokemonType = allDatas.type;
-    console.log(pokemonType);
-    const pokemonWeaknesses = allDatas.weaknesses;
-    console.log(pokemonWeaknesses);*/
+    
 
 

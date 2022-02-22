901435a6ecf5b9dd6132a8405b5b6049266ee0a3
Marione-Pereira <mari.tainara@gmail.com>
2022-02-21 23:28:09 -0300

    add imagens ao carrosel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { example } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import { example } from "./data.js";
 
+import data from "./data/rickandmorty/rickandmorty.js";
+
+const imagens = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+let idx = 0;
+function carrosel() {
+  idx++;
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
+  imagens.style.transform = `translatex(${-idx * 500}px)`;
+}
+setInterval(carrosel, 4000);
 
 console.log(example, data);

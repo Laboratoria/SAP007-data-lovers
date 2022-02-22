72c8a2f04b7be46e81ee49538cea71b77abb95aa
Gabriela Medrado <gabrieladmr@gmail.com>
2022-02-22 11:48:10 -0300

    Commit teste

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
 export const dataRickMorty = () => {
-  console.log(data);;
+  console.log(data);
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
 import { dataRickMorty } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+
+function showAllCards(data) {
+
+  document.getElementById("result").innerHTML = data.map(
+    (dado) => `
+
+        <div class="characters-cards">
+          <img src="${dado.image}">
+          <h1>${dado.name}</h1>
+          <ul class="info-list">
+            <li>Status: ${dado.status}</li>
+            <li>Gênero: ${dado.gender}</li>
+            <li>Espécie: ${dado.species}</li>
+            <li>Origem: ${dado.origin.name}</li>
+          </ul >
+        </div >
+  `
+
+  )
+    .join("");
+}
+showAllCards(data.results);
 
 
 
-//console.log(example, data);
 
+const btn = document.getElementById('btn-submit').addEventListener('click', () => {
+  botaCardsNaTela(arrayPersonagens)
+})
 
-
-const button = document.getElementById("analysis");
-
-button.addEventListener("click", dataRickMorty);

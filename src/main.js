69df79f85e9d47178ddad0d96b14c3ea059c7df1
suelenescorcio <suelen919@gmail.com>
2022-02-22 11:37:55 -0300

    Alterações no footer e no main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
-import { dataRickMorty } from './data.js';
+import { dataRickMorty } from "./data.js";
+import data from "./data/rickandmorty/rickandmorty.js";
 
 
+document.getElementById("analysis").addEventListener("click", () =>{
+  allCards(data.results)
+})
 
-//console.log(example, data);
+function allCards(data) {
+  document.querySelector(".result").innerHTML = data
+  .map(
+    (dado) => `
 
+        <div class="cards">
+          <img src="${dado.image}"></img>
+          <h1>${dado.name}</h1>
+          <ul class="info-list">
+            <li>Status: ${dado.status}</li>
+            <li>Gênero: ${dado.gender}</li>
+            <li>Espécie: ${dado.species}</li>
+            <li>Origem: ${dado.origin}</li>
+            <li>Tipo: ${dado.type}</li>
+          </ul>
+        </div>
+      `
+  );
+  join(" ");
+}
 
-
-const button = document.getElementById("analysis");
-
-button.addEventListener("click", dataRickMorty);

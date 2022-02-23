228c235cf934819d53dbba11c51c3f9619726ac0
lud-ic <mag.ludmila@gmail.com>
2022-02-23 03:36:33 -0300

    feat: show character images and names on screen

--- src/js/pages/characters.js
+++ src/js/pages/characters.js
@@ -1,3 +1,23 @@
+import data from "../../data/ghibli/ghibli.js";
 import { menu } from "../components/header.js";
 
 menu();
+
+function makeCharacterCards(dataAll) {
+  document.getElementById("cardsCharacters").innerHTML = dataAll.films
+    .map((film) =>
+      film.people
+        .map(
+          (character) =>
+            `
+            <section class="cards">
+              <img src="${character.img}"/>
+              <h1>${character.name}</h1>
+            </section>
+            `
+        )
+        .join("")
+    )
+    .join("");
+}
+makeCharacterCards(data);

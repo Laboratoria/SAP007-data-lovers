cf643354f1dcd5ed9ef6103e0fa25858bf0abe46
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-02-23 17:48:10 -0300

    Adicionando comentários com explicações

--- src/js/personagens.js
+++ src/js/personagens.js
@@ -1,19 +1,25 @@
 import data from "../data/ghibli/ghibli.js";
 
-const containerAnimes = document.getElementById("containerCardItem");
+//const que captura o valor do elemento HTML a ser preenchido pela div cardAnime
+const containerAnimes = document.getElementById("containerCardItem"); //const que captura o valor do elemento
 
+//let que armazena a propriedade films de ghibli.js
 let filmesData = data.films;
 
+//loop que percorre toda a propriedade films (todos os filmes)
 for (let i = 0; i < filmesData.length; i++) {
+  //let que acessa os objetos do array contido na propriedade people
   let personagens = filmesData[i].people;
-  
+
+  //loop que percorre cada objeto do array da propriedade people
   for (let j = 0; j < personagens.length; j++) {
+    console.log(personagens);
 
-  console.log(personagens);
-
-  const cardAnime = document.createElement("div");
-  cardAnime.className = "containerCardIndividual";
-  cardAnime.innerHTML = `  
+    //const cardAnime cria um elemento a ser adicionado ao HTML pelo appendChild
+    const cardAnime = document.createElement("div");
+    cardAnime.className = "containerCardIndividual"; //dando uma classe ao elemento criado (cardAnime)
+    //a propriedade innerHTML define, na sintaxe HTML, o conteúdo que preencherá o elemento criado (cardAnime)
+    cardAnime.innerHTML = `  
     <div class="conteinerImagem">
     <p id="titulo"> ${personagens[j].name} </p>
     <img src= '${personagens[j].img}' id="imagem-poster"></img>
@@ -24,10 +30,12 @@ for (let i = 0; i < filmesData.length; i++) {
       <!--<div class="lançamento">${personagens[j].age}</div>-->
       </div>
       <div id="diretor">${personagens[j].specie}</div>
-      `;
+      `; //transforma o conteúdo das propriedades mencioadas em strings
 
-  containerAnimes.appendChild(cardAnime);
-}}
+    containerAnimes.appendChild(cardAnime); //método appendChild adiciona um elemento
+    // filho (cardAnime) ao elemento do HTML pai (containerAnimes)
+  }
+}
 
 document.getElementById("recarregar").addEventListener("click", () => {
   location.reload();

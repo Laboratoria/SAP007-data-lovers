4e6f29bed293f18e7523ff51ba3e266b8f58a562
Marione-Pereira <mari.tainara@gmail.com>
2022-02-24 18:58:07 -0300

    add filtros por especie e barra de pesquisa

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 //DOM, event listeners, event handlers
 
-
-import { ordemAlfabetica } from "./data.js"; 
+import { ordemAlfabetica } from "./data.js";
 import data from "./data/rickandmorty/rickandmorty.js";
-
-
 
 console.log(ordemAlfabetica, data);
 
-const btnAZ = document.getElementById('btn-az')
+const btnAZ = document.getElementById("btn-az");
 
-function buttonAZ (e){
+function buttonAZ(e) {
   e.preventDefault();
   //como retornar o resultado?
 }

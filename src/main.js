7e27e0a1fcb5aaefaef6f72256c89f7add612495
Gabriela Medrado <gabrieladmr@gmail.com>
2022-02-24 13:48:30 -0300

    feat:filtros status e gênero

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { dataRickMorty } from "./data.js";
+import { statusFilter, genderFilter } from "./data.js";
+
 import data from "./data/rickandmorty/rickandmorty.js";
 
 
-document.getElementById("characters").addEventListener("click", () => {
+/*document.getElementById("characters").addEventListener("click", () => {
+  window.location.href = "cards.html"
   allCards(data.results)
-})
+})*/
 
 function allCards(data) {
   document.querySelector(".result").innerHTML = data
@@ -18,12 +20,41 @@ function allCards(data) {
             <li>Status: ${dado.status}</li>
             <li>Gênero: ${dado.gender}</li>
             <li>Espécie: ${dado.species}</li>
-            <li>Origem: ${dado.origin}</li>
+            <li>Origem: ${dado.origin.name}</li>
           </ul>
         </div>
       `
-    );
-  join("");
+    )
+    .join("");
 
 }
+
+
+//Seletores
+
+const lifeSelect = document.getElementById("menu_filter_life");
+const genderSelect = document.getElementById("menu_filter_gender");
+const sortSelect = document.getElementById("menu_sort");
+
+
+//Funções
+function printStatus(e) {
+  const statusResult = statusFilter(data.results, e.target.value);
+  console.log(statusResult)
+  //return allCards(statusResult);
+}
+lifeSelect.addEventListener("change", printStatus);
+
+
+function printGender(e) {
+  const genderResult = genderFilter(data.results, e.target.value);
+  console.log(genderResult);
+  //return allCards(genderResult);
+}
+
+genderSelect.addEventListener("change", printGender);
+
+// event.target.value = buscar diretamente o valor definido no seletor
+
+
 

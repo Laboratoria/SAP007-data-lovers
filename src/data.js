e0c7568d5b3417af90eb0d4e4d4cbb74be5ddabd
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-03-02 20:50:46 -0300

    Alterações no main.js

--- src/data.js
+++ src/data.js
@@ -6,3 +6,72 @@ export const dataPokemon = () => {
     });
   return false;
 };
+
+/*
+<div class="card">
+  <img class="pokedex-open" src="img/pokedex-open.png">
+  <p class="poke-number">N° 001</p>
+  <div class="card-box">
+    <figure class="box-poke-img">
+      <img class="poke-img" src="img/001.png" alt="Pokémon 001 Bulbasaur">
+    </figure>
+    <main class="box-poke-text">
+      <h4 class="poke-title">Exemplo de nome grande</h4>
+      <ul class="poke-items">
+        <span class="poke-item-title">Tipo: </span>
+        <li>Planta</li>
+        <li>Venenoso</li>
+      </ul>
+      <ul class="poke-items">
+        <span class="poke-item-title">Fraqueza:</span>
+        <li>Fogo</li>
+        <li>Gelo</li>
+        <li>Voador</li>
+        <li>Psíquico</li>
+        <li>Gelo</li>
+        <li>Voador</li>
+        <li>Psíquico</li>
+        <li>Gelo</li>
+        <li>Voador</li>
+        <li>Psíquico</li>
+      </ul>
+    </main>
+  </div>
+</div>
+
+
+document
+  .getElementById("confirm-button")
+  .addEventListener("click", showResults);
+
+function showResults(event) {
+  event.preventDefault();
+
+  for (let i = 0; i < data.pokemon.length; i++) {
+    let allDatas = data.pokemon[i];
+    console.log(typeof allDatas);
+
+    Primeira letra do nome maiuscula - deixar para fazer no final para nao atrapalhar o sort() 
+    const alteredName = allDatas.name[0].toUpperCase() + allDatas.name.substring(1);
+
+    // card dos pokemons string
+    const pokemonCard = `
+    <section class="show-the-cards">
+      <div class="img-box">
+        <img src= "${allDatas.img}" alt=${allDatas.name}>
+      </div>
+         
+      <div class="text-box">
+        <p class="poke-name">Nome ${allDatas.name} </p>
+        <p class="poke-number">N° ${allDatas.num} </p>
+        <p class="poke-type">Tipo ${allDatas.type}</p>
+        <p class="poke-weaknesses">Fraqueza ${allDatas.weaknesses}</p>
+      </div>
+    </section>
+    `;
+
+    const sectionResults = document.createElement("section");
+    document.getElementById("result").appendChild(sectionResults);
+    sectionResults.innerHTML = pokemonCard;
+  }
+  */
--- src/main.js
+++ src/main.js
@@ -1,27 +1,69 @@
-import data from "./data/pokemon/pokemon.js";
+//import data from "./data/pokemon/pokemon.js";
 
+/*
+ Pegar os dados importantes
+ Exibir os dados na tela
+ Criar template do card (html)
+ Passar esse template para dentro do loop
+ Jogar de volta para o document a string criada
+ Filtrar os dados por tipo
+ Filtrar os dados por fraqueza
+ Filtrar por nome usando regex
+ Não permitir que tipos e fraquezas estejam disponíveis ao escrever o nome
+ Ordenar os dados por número da pokédex
+ Ordenar os dados por ordem alfabética crescente
+ Ordenar os dados por ordem alfabética decrescente
+ Ordenar por maior quantidade de fraqueza
+ Ordenar por menor quantidade de fraqueza
+ Resetar o filtro
+*/
+var confirmPage = false;
 startPageHome();
 
+function formDataCheckbox(e) {
+  e.preventDefault();
+  const formCheck = document.querySelector(".full-checkbox").elements;
+  let result = "";
+  for (let i = 0; i < formCheck.length; i++) {
+    if (formCheck[i].checked) {
+      result = result + formCheck[i].value + " ";
+      console.log(typeof formCheck[i]);
+    }
+  }
+  document.getElementById("resultado").innerHTML =
+    "Valor(es) selecionado(s) = " + result;
+  //console.log(form.weakness.value);
+  //form.reset();
+}
+
+function startPageFilters() {
+  window.location.href = "filters.html";
+  document
+    .getElementById("confirm-button")
+    .addEventListener("click", formDataCheckbox);
+}
+
 function startPageHome() {
-  heightWindow();
-  screen.orientation.onchange = function (e) {
-    let containerMain = document.querySelector(".main-home");
-    containerMain.style.height = "";
+  do {
     heightWindow();
-  };
+  } while (confirmPage == false);
+  const clickPokedex = document.getElementsByClassName("pokedex-close").onclick;
+  if (clickPokedex == true) {
+    startPageFilters();
+  }
 }
 
 function heightWindow() {
   let heightWindow = Number(window.innerHeight);
-  console.log(heightWindow);
   let heightLogo = Number(document.querySelector(".header-home").scrollHeight);
-  console.log(heightLogo);
   let heightText = Number(document.querySelector(".intro-text").scrollHeight);
-  console.log(heightText);
   let heightTab = Number(document.querySelector(".details-info").scrollHeight);
-  console.log(heightTab);
   let sumAll = heightWindow - (heightLogo + heightText + heightTab);
-  console.log(sumAll);
   let containerMain = document.querySelector(".main-home");
   containerMain.style.height = sumAll + "px";
+  screen.orientation.onchange = function (e) {
+    confirmPage == true;
+    let containerMain = document.querySelector(".main-home");
+    containerMain.style.height = "";
+  };
 }

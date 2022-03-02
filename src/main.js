f028c49dd6c07341b11d1383e40421c84f549b72
MariaLuizaSantana <costamalu123@outlook.com>
2022-03-02 17:55:07 -0300

    htmlatualizadoversao1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/ghibli/ghibli.js';
-// import data from './data/athletes/athletes.js';
+import {} from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+/////IMPRIMIR CARDS NA TELA
+function imprimirCardsTela (data) {
+    document.getElementById('infoCards').innerHTML = data.map((item) =>
+    <div class = "cards">
+        <div class="frenteCards">
+            <img class="cardImg" src="${item.image}" ></img>
+        </div>
+        
+        <div class = "telaText">
+            <ul class = "telaTextCard">
+                <li><strong>Nome:</strong>${item.name}</li>
+                <li><strong>Espécie:</strong>${item.species}</li>
+                <li><strong>Status:</strong>${item.status}</li>
+                <li><strong>Gênero:</strong>${item.gender}</li>
+                <li><strong>Local de Origem:</strong>${item.origin.name}</li>
+            </ul>
+        </div>
+    </div>
+).join(' ')
+}
+imprimirCardsTela(data.results);

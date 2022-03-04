26f6937032b01fe0366eac5c422239a6d9170573
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-03-04 16:10:34 -0300

    Inserindo loop personagens em uma função e tentativa de filtrar

--- src/js/data.js
+++ src/js/data.js
@@ -7,3 +7,8 @@ export function filterDataLançamento(data, condition) {
   let filterLançamento = data.filter((item) => item.release_date === condition);
     return filterLançamento;
 }
+
+export function filterDataGênero(data, condition) {
+  let filterGênero = data.filter((item) => item.gender === condition);
+    return filterGênero;
+}
--- src/js/personagens.js
+++ src/js/personagens.js
@@ -1,4 +1,5 @@
 import data from "../data/ghibli/ghibli.js";
+import { filterDataGênero } from "./data.js";
 
 //const que captura o valor do elemento HTML a ser preenchido pela div cardAnime
 const containerAnimes = document.getElementById("containerCardItem");
@@ -6,20 +7,23 @@ const containerAnimes = document.getElementById("containerCardItem");
 //let que armazena a propriedade films de ghibli.js
 let filmesData = data.films;
 
-//loop que percorre toda a propriedade films (todos os filmes)
-for (let i = 0; i < filmesData.length; i++) {
-  //let que acessa os objetos do array contido na propriedade people
-  let personagens = filmesData[i].people;
+let personaData = filmesData.map((item) => item.people);
+console.log(personaData)
 
-  //loop que percorre cada objeto do array da propriedade people
-  for (let j = 0; j < personagens.length; j++) {
-    console.log(personagens);
+filmesData.forEach(mostrarPersonagens); //mapeando array films com método de callback forEach
 
-    //const cardAnime cria um elemento a ser adicionado ao HTML pelo appendChild
-    const cardAnime = document.createElement("section");
-    cardAnime.className = "container-card-individual"; //dando uma classe ao elemento criado (cardAnime)
-    //a propriedade innerHTML define, na sintaxe HTML, o conteúdo que preencherá o elemento criado (cardAnime)
-    cardAnime.innerHTML = `  
+function mostrarPersonagens() {
+  for (let i = 0; i < filmesData.length; i++) {
+    //let que acessa os objetos do array contido na propriedade people
+    let personagens = filmesData[i].people;
+  
+    //loop que percorre cada objeto do array da propriedade people
+    for (let j = 0; j < personagens.length; j++) {
+      console.log(personagens);
+
+  const cardAnime = document.createElement("section");
+  cardAnime.className = "container-card-individual";
+  cardAnime.innerHTML = `
     <div class="conteinerImagem">
     <p id="titulo"> ${personagens[j].name} </p>
     <img src= '${personagens[j].img}' id="imagem-poster"></img>
@@ -29,14 +33,18 @@ for (let i = 0; i < filmesData.length; i++) {
       <div class="container-info">
       <!--<div class="lançamento">${personagens[j].age}</div>-->
       </div>
-      <div id="diretor">${personagens[j].specie}</div>
-      `; //o template ${} transforma o conteúdo das propriedades mencioadas em strings
-
-    containerAnimes.appendChild(cardAnime); //método appendChild adiciona um elemento
-    // filho (cardAnime) ao elemento do HTML pai (containerAnimes)
-  }
-}
+      <div id="diretor">${personagens[j].gender}</div>
+      `;
+  containerAnimes.appendChild(cardAnime);
+}}}
 
 document.getElementById("recarregar").addEventListener("click", () => {
   location.reload();
+});
+
+document.getElementById("filtroGêneroItem").addEventListener("change", () => {
+  let gênero = document.querySelector(".filtro-gênero");
+  let filterItem = filterDataGênero(personaData, gênero.value);
+  containerAnimes.innerHTML = "";
+  filterItem.forEach(mostrarPersonagens);
 });

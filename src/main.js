737f4b2a7754ebb324220487b32b777e279334d9
MariaLuizaSantana <costamalu123@outlook.com>
2022-03-04 10:18:04 -0300

    mudançasHTMLCSS

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 /////IMPRIMIR CARDS NA TELA
 function imprimirCardsTela (data) {
-    document.getElementById('infoCards').innerHTML = data.map((item) =>
+    document.getElementById('infoCards').innerHTML = data.map((item) => `
     <div class = "cards">
         <div class="frenteCards">
-            <img class="cardImg" src="${item.image}" ></img>
+            <img class="cardImg" src="${item.image}"></img>
         </div>
         
         <div class = "telaText">
@@ -15,10 +15,9 @@ function imprimirCardsTela (data) {
                 <li><strong>Espécie:</strong>${item.species}</li>
                 <li><strong>Status:</strong>${item.status}</li>
                 <li><strong>Gênero:</strong>${item.gender}</li>
-                <li><strong>Local de Origem:</strong>${item.origin.name}</li>
             </ul>
         </div>
-    </div>
+    </div>`
 ).join(' ')
 }
 imprimirCardsTela(data.results);

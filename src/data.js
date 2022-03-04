851bffbb85b35636d2d4db9c6f90e1b415646981
Fernanda741 <fernanda2pc@gmail.com>
2022-03-04 16:51:37 -0300

    Inclusão do JS dos botões

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
+export const ascAlphabeth = (data) => data.sort((a,b) =>
+((a.name > b.name) ? 1: -1));
+
+export const descAlphabeth = (data) => data.sort((a,b) =>
+  ((a.name > b.name) ? -1: 1));
 
 
-export const example = () => {
-  return 'example';
-};
+  export const statusFilter = (allCharacters, statusOption) => {
+    //console.log (allCharacters, statusOption);
+    return allCharacters.filter(character => character.status === statusOption)
+  };
 
-export const anotherExample = () => {
-  return 'OMG';
+  export const speciesFilter = (allCharacters, speciesOption) => {
+     return allCharacters.filter(character => character.species === speciesOption)
+  };
+
+  export const genderFilter = (allCharacters, genderOption) => {
+   return allCharacters.filter(character => character.gender === genderOption)
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { example } from './data.js';
+import { ascAlphabeth, descAlphabeth, speciesFilter, statusFilter, genderFilter} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+
 const showCards = document.getElementById("cards")
-const cardPrint = data.results.map((item) => {
-  return `<div class="card-container">
+const cardPrint = data.results
+function showInfos(cardPrint) {
+  showCards.innerHTML = cardPrint.map(item =>
+         `<div class="card-container">
             <div class="card-img">
               <img src="${item.image}">
             </div>
@@ -14,9 +17,48 @@ const cardPrint = data.results.map((item) => {
               <p>Gênero:${item.gender}</p>
             </div>
           </div>
-`}
-)
-showCards.innerHTML = cardPrint
+          `
+   ).join('')
+}
+
+showInfos(cardPrint)
 
 
+const statusOption = document.querySelector(".status-btn");
+const speciesOption = document.querySelector(".species-btn");
+const genderOption = document.querySelector(".gender-btn");
+const searchByName = document.getElementById("name-search");
 
+
+function ascOrder(){
+  return showInfos(ascAlphabeth(data.results));
+}
+document.getElementById("btn-az").addEventListener("click", ascOrder);
+
+function descOrder(){
+  return showInfos(descAlphabeth(data.results));
+}
+document.getElementById("btn-za").addEventListener("click", descOrder);
+
+
+function filterByStatus () {
+  const statusValue = statusOption.value;
+  const charactersStatus = statusFilter (data.results, statusValue);
+  //console.log (charactersStatus)
+  return showInfos(charactersStatus);
+}
+statusOption.addEventListener("change", filterByStatus);
+
+function filterBySpecies () {
+  const specieValue = speciesOption.value;
+  const charactersSpecies = speciesFilter (data.results, specieValue);
+  return showInfos (charactersSpecies);
+}
+speciesOption.addEventListener("change", filterBySpecies);
+
+function filterByGender () {
+  const genderValue = genderOption.value;
+  const charactersGender = genderFilter (data.results, genderValue);
+  return showInfos(charactersGender);
+}
+genderOption.addEventListener("change", filterByGender);

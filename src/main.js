4253761dd799e1851d369da7835121dcb1c614d1
JackeAguiar <jackelineaguiar000@gmail.com>
2022-03-07 17:38:03 -0300

    tentativa de colocar o card na tela

--- src/main.js
+++ src/main.js
@@ -3,24 +3,27 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
 
-const pokemons = data.pokemon.slice(1, 151),
-const cardsPokemon = getElementById("cards")
+const pokemons = data.pokemon.slice(0, 251)
+
+const cardsPokemon = document.getElementById("cards");
+
+cardsPokemons(pokemons);
 
 
-function cardsPokemon(arrayPokemon){
+function cardsPokemons(arrayPokemon){
     let cards = "";
-    arrayPokemon.map(actualPokemon) => {
+    arrayPokemon.map(actualPokemon => {
         const types = actualPokemon.types;
         cards +=
-            `<div class="card" '${types[0]}'>
+            `<div class="card" '${types['']}'>
         <img class="card-image " alt="${actualPokemon.name}" src="https://www.serebii.net/pokemongo/pokemon/${actualPokemon.num}.png" />
         <h2 class="card-title"> ${actualPokemon.num}. ${actualPokemon.name} </h2>
         <p class="card-subtitle" id="card-subtitle">${actualPokemon.size.height} | ${actualPokemon.size.weight} </p>
         <p class="card-subtitle2" id="card-subtitle2">${types.join(' | ')} </p>
-        <p class="card-subtitle3" id="card-subtitle">${actualPokemonl.generation.name} </p>
-        </div>`;
+        <p class="card-subtitle3" id="card-subtitle">${actualPokemon.generation.name} </p>
+        </div>`
     });
-
-    cardPokemon.innerHTML = cards;
+     console.log(arrayPokemon)
+    cardsPokemon.innerHTML = cards;
 }
-cardsPokemon(pokemons);
+

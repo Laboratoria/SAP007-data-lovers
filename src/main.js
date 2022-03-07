26badfcbf4d1145153d69edd107042ec768f1a6f
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-03-07 18:52:51 -0300

    js print card

--- src/main.js
+++ src/main.js
@@ -1,14 +1,51 @@
-import { example } from './data.js';
+import {
+  example
+} from './data.js';
 
 import data from './data/ghibli/ghibli.js';
+
+
 
 const buttonAbout = document.querySelector("#about");
 const main = document.querySelector('.main')
 const pageAbout = document.querySelector('#pageAbout')
 
-buttonAbout.addEventListener('click',() => {
+buttonAbout.addEventListener('click', () => {
   main.classList.add("hidden")
   pageAbout.classList.remove("hidden")
 })
 
+
+
+function printCards(banana) {
+  document.getElementById('cardsFilms').innerHTML = banana.map((item) =>
+      `
+  <section = "cardFlip">
+  <figure>
+  <img src = "${item.poster}" class = "poster">
+  </figure>
+
+    <p class = "infoFilms"><strong>Title: </strong>${item.title}</p><br>
+    <p class = "infoFilms"><strong>Description: </strong>${item.description}</p><br>
+    <p class = "infoFilms"><strong>Director: </strong>${item.director}<p><br>
+  </section>
+    `
+    )
+    .join('');
+}
+
+printCards(data.films)
 console.log(example, data);
+
+
+/* `
+    <figure>
+  <img src = "${item.poster} class = "poster">
+  </figure>
+
+  <section>
+    <p class = "infoFilms"><strong>Title:</strong>${item.title}</p><br>
+    <p class = "infoFilms"><strong>Description</strong>${item.description}</p><br>
+    <p class = "infoFilms"><strong>Director</strong>${item.director}<p><br>
+  </section>
+    `</p></p>*/

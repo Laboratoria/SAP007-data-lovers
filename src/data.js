8bf7af23e88da886b5cef27bdf8938bc8b7d35c9
Gregisa <isabela.bonander@gmail.com>
2022-03-07 16:17:38 -0300

    js-botao limpar/input name/calculo

--- src/data.js
+++ src/data.js
@@ -1,19 +1,28 @@
-export const ascAlphabeth = (data) => data.sort((a,b) =>
-((a.name > b.name) ? 1: -1));
+export const ascAlphabeth = (data) => data.sort((a, b) =>
+  ((a.name > b.name) ? 1 : -1));
 
-export const descAlphabeth = (data) => data.sort((a,b) =>
-  ((a.name > b.name) ? -1: 1));
+export const descAlphabeth = (data) => data.sort((a, b) =>
+  ((a.name > b.name) ? -1 : 1));
 
 
-  export const statusFilter = (allCharacters, statusOption) => {
-    //console.log (allCharacters, statusOption);
-    return allCharacters.filter(character => character.status === statusOption)
-  };
+export const statusFilter = (allCharacters, statusOption) => {
+  //console.log (allCharacters, statusOption);
+  return allCharacters.filter(character => character.status === statusOption)
+};
 
-  export const speciesFilter = (allCharacters, speciesOption) => {
-     return allCharacters.filter(character => character.species === speciesOption)
-  };
+export const speciesFilter = (allCharacters, speciesOption) => {
+  return allCharacters.filter(character => character.species === speciesOption)
+};
 
-  export const genderFilter = (allCharacters, genderOption) => {
-   return allCharacters.filter(character => character.gender === genderOption)
+export const genderFilter = (allCharacters, genderOption) => {
+  return allCharacters.filter(character => character.gender === genderOption)
+};
+
+export const nameFilter = (allCharacters, searchByName) => allCharacters.filter(character => {
+  return character.name.toLowerCase().includes(searchByName.toLowerCase());
+});
+
+export const calculator =  (allCharacters, categoryResult) => {
+  const percentage = Math.round((categoryResult * 100) / allCharacters);
+  return percentage;
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { ascAlphabeth, descAlphabeth, speciesFilter, statusFilter, genderFilter} from './data.js';
+import { ascAlphabeth, descAlphabeth, speciesFilter, statusFilter, genderFilter, nameFilter, calculator } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 
-const showCards = document.getElementById("cards")
-const cardPrint = data.results
+const showCards = document.getElementById("cards");
+const cardPrint = data.results;
 function showInfos(cardPrint) {
   showCards.innerHTML = cardPrint.map(item =>
          `<div class="card-container">
@@ -16,49 +16,80 @@ function showInfos(cardPrint) {
               <p>Espécie:${item.species}</p>
               <p>Gênero:${item.gender}</p>
             </div>
-          </div>
-          `
-   ).join('')
+          </div>`
+  ).join('')
 }
 
-showInfos(cardPrint)
+showInfos(cardPrint);
 
 
 const statusOption = document.querySelector(".status-btn");
 const speciesOption = document.querySelector(".species-btn");
 const genderOption = document.querySelector(".gender-btn");
 const searchByName = document.getElementById("name-search");
+const clearFilters = document.getElementById("btn-clear");
 
-
-function ascOrder(){
-  return showInfos(ascAlphabeth(data.results));
+function ascOrder() {
+  return showInfos(ascAlphabeth(cardPrint));
 }
 document.getElementById("btn-az").addEventListener("click", ascOrder);
 
-function descOrder(){
-  return showInfos(descAlphabeth(data.results));
+function descOrder() {
+  return showInfos(descAlphabeth(cardPrint));
 }
 document.getElementById("btn-za").addEventListener("click", descOrder);
 
+function showCalculation(cardPrint) {
+  const categoryResult = document.getElementById("calculation-result");
+  categoryResult.innerHTML = `<p>O total de personagens dessa categoria é
+  ${cardPrint}<p>`
+  categoryResult.style.display = 'inline-block'
+}
 
-function filterByStatus () {
+function filterByStatus() {
   const statusValue = statusOption.value;
-  const charactersStatus = statusFilter (data.results, statusValue);
-  //console.log (charactersStatus)
+  const charactersStatus = statusFilter(cardPrint, statusValue);
+  const percentageStatus = `${calculator(cardPrint.length, charactersStatus.length)}% dos personagens`
+  showCalculation(percentageStatus);
   return showInfos(charactersStatus);
 }
 statusOption.addEventListener("change", filterByStatus);
 
-function filterBySpecies () {
+function filterBySpecies() {
   const specieValue = speciesOption.value;
-  const charactersSpecies = speciesFilter (data.results, specieValue);
-  return showInfos (charactersSpecies);
+  const charactersSpecies = speciesFilter(cardPrint, specieValue);
+  const percentageSpecies = `${calculator(cardPrint.length, charactersSpecies.length)}% dos personagens`
+  showCalculation(percentageSpecies);
+  return showInfos(charactersSpecies);
 }
 speciesOption.addEventListener("change", filterBySpecies);
 
-function filterByGender () {
+function filterByGender() {
   const genderValue = genderOption.value;
-  const charactersGender = genderFilter (data.results, genderValue);
+  const charactersGender = genderFilter(cardPrint, genderValue);
+  const percentageGender = `${calculator(cardPrint.length, charactersGender.length)}% dos personagens`
+  showCalculation(percentageGender);
   return showInfos(charactersGender);
 }
 genderOption.addEventListener("change", filterByGender);
+
+function filterByName() {
+  const nameValue = searchByName.value;
+  const charactersName = nameFilter(cardPrint, nameValue);
+  return showInfos(charactersName);
+}
+searchByName.addEventListener("keyup", filterByName);
+
+function clearAll(){
+  window.location.reload();
+}
+clearFilters.addEventListener("click", clearAll);
+
+/*function showCalculation() {
+  const categoryResult = document.getElementById("calculation-result");
+  categoryResult.innerHTML = `<p> O total de personagens dessa categoria é
+  ${calculator(filterByGender)}<p>`
+}
+showCalculation.addEventListener("change", calculator);*/
+
+

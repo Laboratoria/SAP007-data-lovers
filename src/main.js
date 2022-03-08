1ffd022a2ae11687b238a31c14d55bfd49e7a785
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-03-08 17:55:39 -0300

    cards-css-films/about-doc

--- src/main.js
+++ src/main.js
@@ -9,29 +9,39 @@ import data from './data/ghibli/ghibli.js';
 const buttonAbout = document.querySelector("#about");
 const main = document.querySelector('.main')
 const pageAbout = document.querySelector('#pageAbout')
+const mainFilms = document.querySelector('.mainFilms')
 
 buttonAbout.addEventListener('click', () => {
   main.classList.add("hidden")
+  mainFilms.classList.add("hidden")
   pageAbout.classList.remove("hidden")
 })
 
 
 
-function printCards(banana) {
-  document.getElementById('cardsFilms').innerHTML = banana.map((item) =>
-      `
-  <section = "cardFlip">
-  <figure>
-  <img src = "${item.poster}" class = "poster">
-  </figure>
-
-    <p class = "infoFilms"><strong>Title: </strong>${item.title}</p><br>
-    <p class = "infoFilms"><strong>Description: </strong>${item.description}</p><br>
-    <p class = "infoFilms"><strong>Director: </strong>${item.director}<p><br>
-  </section>
+function printCards(films) {
+  document.getElementById('cardsFilms').innerHTML = films.map((item) =>
+    `
+  <div class="mainCards">
+    <div class="cardFlip">
+      <div class="front">
+        <figure>
+          <img src = "${item.poster}" class = "poster">
+        </figure>
+      </div>
+      <div class="back">
+        <p class = "infoFilms"><strong>Title: </strong>${item.title}</p><br>
+        <p class = "infoFilms"><strong>Release Date: </strong>${item.release_date}</p><br>
+        <p class = "infoFilms"><strong>Description: </strong>${item.description}</p><br>
+        <p class = "infoFilms"><strong>Director: </strong>${item.director}<p><br>
+        <p class = "infoFilms"><strong>Producer: </strong>${item.producer}<p><br>
+        <p class = "infoFilms"><strong>Score: </strong>${item.rt_score}<p><br>
+      </div>
+     </div>
+    </div>
     `
     )
-    .join('');
+  .join('');
 }
 
 printCards(data.films)

8f0233dc30f98b063d5a6307e188d9ad315ca7db
Larasantos97 <larissa_reis97@hotmail.com>
2022-03-08 18:01:29 -0300

    Primeiras-funcionalidades atletas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { example } from './data.js';
 import data from './data/athletes/athletes.js';
-
 console.log(example, data);
+
+
+/*const cartasPaíses = data.athletes.map((país)=>{
+  return `<div><p class="conteudo-países">${país.team}
+  </p> <p class="conteudo-países">${país.name}</p></div>`})
+
+console.log (cartasPaíses);
+document.getElementById("cards-países").innerHTML = cartasPaíses;*/
+
+

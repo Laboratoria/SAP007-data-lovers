2db0fa858d9e542bffa7338f37d9a129434b378d
Helena <nannayusuf@gmail.com>
2022-03-08 17:11:38 -0300

    mudanças js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
 import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+/*const containerList = document.getElementById("list")
+const pokemonModal = document.getElementById('miModal');
 
-const pokemons = data.pokemon.slice(1, 151),
-const cardsPokemon = getElementById("cards")
+let body = document.getElementsByTagName("body")[0];
+
+const showData = function (pokemonArray) {
+  containerList.innerHTML = ""
 
 
-function cardsPokemon(arrayPokemon){
-    let cards = "";
-    arrayPokemon.map(actualPokemon) => {
-        const types = actualPokemon.types;
-        cards +=
-            `<div class="card" '${types[0]}'>
-        <img class="card-image " alt="${actualPokemon.name}" src="https://www.serebii.net/pokemongo/pokemon/${actualPokemon.num}.png" />
-        <h2 class="card-title"> ${actualPokemon.num}. ${actualPokemon.name} </h2>
-        <p class="card-subtitle" id="card-subtitle">${actualPokemon.size.height} | ${actualPokemon.size.weight} </p>
-        <p class="card-subtitle2" id="card-subtitle2">${types.join(' | ')} </p>
-        <p class="card-subtitle3" id="card-subtitle">${actualPokemonl.generation.name} </p>
-        </div>`;
-    });
-
-    cardPokemon.innerHTML = cards;
-}
-cardsPokemon(pokemons);
+  pokemonArray.forEach( pokemon => {
+    const item = document.createElement('li')
+    item.className = 'pokemon '
+    item.id = pokemon.num
+    containerList.appendChild(item).innerHTML = `
+         <span class="number-prefix ${pokemon.type[0]}">${pokemon.num}</span>
+         <section class="imgCard">
+           <figure id="pokemon-${pokemon.num}" class="card">
+             <p class="name">${pokemon.name}</p>
+             <img src="${pokemon.img}" class="imgPokemon">
+           </figure>
+         </section>
+         <div class="type">${pokemon.type.map(type => '<span class="' + type + '">'+ type +'</span>').join('')}</div>/*  */

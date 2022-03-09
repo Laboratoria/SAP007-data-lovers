bbd8507c14c7cf77f879d9843125273e966a7313
Helena <nannayusuf@gmail.com>
2022-03-09 17:39:51 -0300

    add sort filter

--- src/data.js
+++ src/data.js
@@ -1,11 +1,16 @@
-// estas funciones son de ejemplo
+let newDataPokemon = JSON.parse(JSON.stringify(datasetToUse));
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-
+    newDataPokemon.forEach((pokemonCharacter, index) => {
+        let attack = parseInt(pokemonCharacter.stats['base-attack']);
+        let defense = parseInt(pokemonCharacter.stats['base-defense']);
+        let stamina = parseInt(pokemonCharacter.stats['base-stamina']);
+        let sumStats = attack+defense+stamina;
+        let meanStats = Math.round((attack+defense+stamina)/3);
+        let newDataPokemonObject = {
+            "sum-stats": sumStats,
+            "mean-stats": meanStats,
+        }
+        newDataPokemon[index] = Object.assign(pokemonCharacter,newDataPokemonObject);
+    });
+    return newDataPokemon
+}

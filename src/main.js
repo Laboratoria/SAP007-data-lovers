ee8c84911ca5ccec2854be8fc6ab93a8bd23f055
IsaSoaresFr <isabelaa.soares@outlook.com>
2022-03-10 02:29:00 -0300

    Adicionei os filtros de genero, especie

--- src/main.js
+++ src/main.js
@@ -27,4 +27,55 @@ function showInfos(arrayRickAndMorty) {
 showInfos(arrayRickAndMorty)
 
 
+const selectStatus = document.getElementById("selectFilterLife")
+
+selectStatus.addEventListener("change", (event) => {
+  const newArrayStatus = arrayRickAndMorty.filter((item)=> {
+   return item.status === event.target.value
+  })
+  console.log(event.target.value)
+  console.log(newArrayStatus)
+  showInfos(newArrayStatus)
+})
+
+const selectSpecies = document.getElementById("selectFilterSpecies")
+
+selectSpecies.addEventListener("change", (event) => {
+  const newArraySpecies = arrayRickAndMorty.filter((item)=> {
+  return item.species === event.target.value
+ })
+  console.log(event.target.value)
+  console.log(newArraySpecies)
+  showInfos(newArraySpecies)
+})
+
+const selectGender = document.getElementById("selectFilterGender")
+
+selectGender.addEventListener("change", (event)=> {
+  const newArrayGender = arrayRickAndMorty.filter((item)=> {
+    return item.gender === event.target.value
+    })
+   console.log(event.target.value)
+   console.log(newArrayGender)
+  showInfos(newArrayGender)
+})
+
+
+//o filtro de origem ainda não está funcionando
+const selectOrigin = document.getElementById("selectFilterOrigin")
+
+selectOrigin.addEventListener("change", (event) => {
+  const newArrayOrigin = arrayRickAndMorty.filter((item) => {
+    return item.origin['name'] === event.target.value
+  })
+  console.log(event.target.value)
+  console.log(newArrayOrigin)
+  showInfos(newArrayOrigin)
+})
+
+
+
+
+
+
 

af81fab0921ef7a5da6fa3358b07e018a53889e9
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-03-11 13:57:02 -0300

    print dos filmes na tela

--- src/data.js
+++ src/data.js
@@ -7,3 +7,6 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,30 @@ import { example } from './data.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
- import data from './data/ghibli/ghibli.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/athletes/athletes.js';
 
 console.log(example, data);
+
+const filmes = data.films;
+
+function printar(filmes) {
+
+  document.getElementById("cards").innerHTML = filmes.map((cards) =>
+    `
+    <div class = "cards">
+      <h1>  ${cards.title}</h1>
+      <p> ${cards.description}</p>
+      <p> ${cards.release_date}</p>
+      <img src = " ${cards.poster}" >
+    </div>
+
+    `
+  ).join("")
+
+}
+
+printar(filmes)
+
+
+console.log(printar(filmes))

b730f7823e138be3772086a180039b3f1ab33941
Larasantos97 <larissa_reis97@hotmail.com>
2022-03-18 16:22:18 -0300

    Criando filtros e ordenação

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-//import { example } from './data.js';
-import data from './data/athletes/athletes.js';
+/*import {filterAtletas} from './data.js';*/
+/*import data from './data/athletes/athletes.js';*/
 
 
-const cartasAtletas = data.athletes.map((atletas)=>{
-  return `<div class="flex-item-atleta"><ol class = "atletas-conteiner"><li class="conteudo-atletas">${atletas.name}
-  </li> <li class="conteudo-atletas">${atletas.team}</li>
-  <li class="conteudo-atletas">${atletas.sport}</li>
-  <li class="conteudo-atletas">${atletas.medal}</li></ol></div>`})
 
-document.getElementById("cards-atletas").innerHTML = cartasAtletas;
+
+
+

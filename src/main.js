8de9e4971cc1cc86b7967f23c811f484f6b6b2e1
Daiane Gonçalves <daianeecs@icloud.com>
2022-03-18 08:37:19 -0300

    AtualizaoFlip

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 const showPersonagens = document.getElementById("card")
 const arrayRickAndMorty = data.results
+let cardsAtuais = arrayRickAndMorty
 
 function showInfos(arrayRickAndMorty) {
   showPersonagens.innerHTML = arrayRickAndMorty.map(item =>
     `<div class="card">
+        <div class="front">
           <div class="name-title">
             <p class="title"> ${item.name}</p>
           </div>
@@ -18,48 +20,65 @@ function showInfos(arrayRickAndMorty) {
             <p> <b>Espécie:</b>${item.species}</p>
             <p> <b>Gênero:</b>${item.gender}</p>
           </div>
-          </div>`
+       </div>
+       <div class="back">
+       <div class="name-title">
+       <p class="title"> ${item.name}</p>
+        </div>
+        <div class="card-img">
+        <img src="${item.image}" class="imgcard" position="center" width="174px" height="170px" border-radius="5%" />
+      </div>
+        <b>Origem:</b> ${item.origin.url}</a>
+        <b>Localização:</b>${item.location.url}</p>
+        <p><b>Epsódios que aparece:</b> ${item.episode.lenght}</p>
+        </div>
+       </div>`
 
   ).join('')
+
 }
 
-showInfos(arrayRickAndMorty)
+showInfos(cardsAtuais)
 //filtros status
 const selectStatus = document.getElementById("selectFilterLife")
 
 selectStatus.addEventListener("change", (event) => {
-  const newArrayStatus = arrayRickAndMorty.filter((item) => {
+  cardsAtuais = cardsAtuais.filter((item) => {
     return item.status === event.target.value
   })
-  showInfos(newArrayStatus)
+
+  showInfos(cardsAtuais)
 })
+
 //filtroespecie
 const selectSpecies = document.getElementById("selectFilterSpecies")
 
 selectSpecies.addEventListener("change", (event) => {
-  const newArraySpecies = arrayRickAndMorty.filter((item) => {
+  cardsAtuais = cardsAtuais.filter((item) => {
     return item.species === event.target.value
   })
-  showInfos(newArraySpecies)
+  showInfos(cardsAtuais)
 })
 //filtroGenero
 const selectGender = document.getElementById("selectFilterGender")
 
 selectGender.addEventListener("change", (event) => {
-  const newArrayGender = arrayRickAndMorty.filter((item) => {
+  cardsAtuais = cardsAtuais.filter((item) => {
     return item.gender === event.target.value
   })
-  showInfos(newArrayGender)
+  showInfos(cardsAtuais)
 })
 //função ordenar A a Z
-function ordemNameA() {
-  return showInfos(ordemNameAA(arrayRickAndMorty));
+function ordemNameA(event) {
+  event.preventDefault()
+  return showInfos(ordemNameAA(cardsAtuais));
 }
 
 document.getElementById("btn-ordemA").addEventListener("click", ordemNameA);
 //função ordenar Z a A
-function ordemNameB() {
-  return showInfos(ordemNameBB(arrayRickAndMorty));
+function ordemNameB(event) {
+  event.preventDefault()
+  return showInfos(ordemNameBB(cardsAtuais));
 }
 document.getElementById("btn-ordemZ").addEventListener("click", ordemNameB);
 //função limpar
@@ -72,20 +91,19 @@ clearFilters.addEventListener("click", clearAll);
 
 const searchBar = document.getElementById('searchBar');
 
-console.log(searchBar);
 searchBar.addEventListener('keyup', (e) => {
   const searchString = e.target.value;
-  const filteredCharacteres = arrayRickAndMorty.filter(item => {
+  cardsAtuais = cardsAtuais.filter(item => {
     return item.name.includes(searchString) ||
       item.status.includes(searchString) ||
       item.species.includes(searchString) ||
       item.gender.includes(searchString)
   });
-  console.log(filteredCharacteres)
-  showInfos(filteredCharacteres)
+  showInfos(cardsAtuais)
 });
 
-var btn = document.querySelector("#voltarAoTopo");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
+//const calculoStatus = document.getElementById("calculoAgreStatus");
+//calculoStatus.innerHTML = ${item.status.reduce((acc,curr) => acc+curr} 0)
+// return acc + curr.
+// return calculoStatus/cardsAtuais.length
+//${item.status.reduce((acc,curr) => acc+curr, 0)}</h2><h3> A média de personagens é: ${item.status.reduce((acc,curr) => acc+curr, 0)/cardsAtuais.length}`

29dfbb93a7af7740e5c4942ad641b2751605a19f
IsaSoaresFr <isabelaa.soares@outlook.com>
2022-03-20 12:41:19 -0300

    calculoAgregado

--- src/data.js
+++ src/data.js
@@ -1,29 +1,32 @@
+<<<<<<< HEAD
 
 
+=======
+//data tem q ser um array de objetos e
+>>>>>>> 7007c254acb3faa0fb5b80a74c52b96258976ffd
 export const ordemNameAA = (data) => data.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    if (nameA > nameB) {
-        return 1;
-    }
-    if (nameA < nameB) {
-        return -1;
-    }
-    return 0;
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+  if (nameA > nameB) {
+    return 1;
+  }
+  if (nameA < nameB) {
+    return -1;
+  }
+  return 0;
 });
 
 export const ordemNameBB = (data) => data.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    if (nameA > nameB) {
-        return -1;
-    }
-    if (nameA < nameB) {
-        return 1;
-    }
-    return 0;
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+  if (nameA > nameB) {
+    return -1;
+  }
+  if (nameA < nameB) {
+    return 1;
+  }
+  return 0;
 });
-
 
 
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 const showPersonagens = document.getElementById("card")
 const arrayRickAndMorty = data.results
+let cardsAtuais = arrayRickAndMorty
 
 function showInfos(arrayRickAndMorty) {
   showPersonagens.innerHTML = arrayRickAndMorty.map(item =>
     `<div class="card">
+        <div class="front">
           <div class="name-title">
             <p class="title"> ${item.name}</p>
           </div>
@@ -18,93 +20,125 @@ function showInfos(arrayRickAndMorty) {
             <p> <b>Espécie:</b>${item.species}</p>
             <p> <b>Gênero:</b>${item.gender}</p>
           </div>
-          </div>`
+       </div>
+       <div class="back">
+       <div class="name-title">
+       <p class="title"> ${item.name}</p>
+        </div>
+        <div class="card-img">
+        <img src="${item.image}" class="imgcard" position="center" width="174px" height="170px" border-radius="5%" />
+      </div>
+        <b>Origem:</b> ${item.origin.url}</a>
+        <b>Localização:</b>${item.location.url}</p>
+        <p><b>Epsódios que aparece:</b> ${item.episode.lenght}</p>
+        </div>
+       </div>`
 
   ).join('')
+
+  showInfos(arrayRickAndMorty)
 }
+showInfos(cardsAtuais)
 
-showInfos(arrayRickAndMorty)
+  //filtros status
+  const selectStatus = document.getElementById("selectFilterLife")
 
-//filtros status
-const selectStatus = document.getElementById("selectFilterLife")
+  function calculo(numero, numero2){
+    let statS = numero/ numero2 *100;
+    document.getElementById("statistics").innerText = `${statS.toFixed(1)}% dos personagens corresponde a categoria escolhida`
+    console.log(showStats)
+  }
 
-function calculo(numero, numero2){
-  let statS = numero/ numero2 *100;
-  document.getElementById("statistics").innerText = `${statS.toFixed(1)}% dos personagens corresponde a categoria escolhida`
-  console.log(showStats)
-}
+  selectStatus.addEventListener("change", (event) => {
 
-selectStatus.addEventListener("change", (event) => {
-  const newArrayStatus = arrayRickAndMorty.filter((item) => {
-    let carD = item.status === event.target.value
-    return carD
+    const newArrayStatus = arrayRickAndMorty.filter((item) => {
+      let carD = item.status === event.target.value
+      return carD
+    })
+    calculo(newArrayStatus.length, arrayRickAndMorty.length)
+    showInfos(newArrayStatus)
+
+    cardsAtuais = cardsAtuais.filter((item) => {
+      return item.status === event.target.value
+    })
+
+    showInfos(cardsAtuais)
+
   })
-  calculo(newArrayStatus.length, arrayRickAndMorty.length)
-  showInfos(newArrayStatus)
-})
 
-//filtroespecie
-const selectSpecies = document.getElementById("selectFilterSpecies")
+  //filtroespecie
+  const selectSpecies = document.getElementById("selectFilterSpecies")
 
-selectSpecies.addEventListener("change", (event) => {
-  const newArraySpecies = arrayRickAndMorty.filter((item) => {
-    return item.species === event.target.value
+  selectSpecies.addEventListener("change", (event) => {
+    cardsAtuais = cardsAtuais.filter((item) => {
+      return item.species === event.target.value
+    })
+
+    calculo(newArraySpecies.length, arrayRickAndMorty.length)
+    showInfos(newArraySpecies)
+
+    showInfos(cardsAtuais)
+
   })
-  calculo(newArraySpecies.length, arrayRickAndMorty.length)
-  showInfos(newArraySpecies)
-})
-//filtroGenero
-const selectGender = document.getElementById("selectFilterGender")
+  //filtroGenero
+  const selectGender = document.getElementById("selectFilterGender")
 
-selectGender.addEventListener("change", (event) => {
-  const newArrayGender = arrayRickAndMorty.filter((item) => {
-    return item.gender === event.target.value
+  selectGender.addEventListener("change", (event) => {
+    cardsAtuais = cardsAtuais.filter((item) => {
+      return item.gender === event.target.value
+    })
+
+    calculo(newArrayGender.length, arrayRickAndMorty.length)
+    showInfos(newArrayGender)
+
+    showInfos(cardsAtuais)
+
   })
-  calculo(newArrayGender.length, arrayRickAndMorty.length)
-  showInfos(newArrayGender)
-})
 
-//função ordenar A a Z
-function ordemNameA() {
-  return showInfos(ordemNameAA(arrayRickAndMorty));
-}
+  //função ordenar A a Z
+  function ordemNameA(event) {
+    event.preventDefault()
+    return showInfos(ordemNameAA(cardsAtuais));
+  }
 
-document.getElementById("btn-ordemA").addEventListener("click", ordemNameA);
-//função ordenar Z a A
-function ordemNameB() {
-  return showInfos(ordemNameBB(arrayRickAndMorty));
-}
-document.getElementById("btn-ordemZ").addEventListener("click", ordemNameB);
-//função limpar
-const clearFilters = document.getElementById("btn-limpar");
-function clearAll() {
-  window.location.reload();
-}
-clearFilters.addEventListener("click", clearAll);
-//pesquisar por nome
+  document.getElementById("btn-ordemA").addEventListener("click", ordemNameA);
+  //função ordenar Z a A
+  function ordemNameB(event) {
+    event.preventDefault()
+    return showInfos(ordemNameBB(cardsAtuais));
+  }
+  document.getElementById("btn-ordemZ").addEventListener("click", ordemNameB);
+  //função limpar
+  const clearFilters = document.getElementById("btn-limpar");
+  function clearAll() {
+    window.location.reload();
+  }
+  clearFilters.addEventListener("click", clearAll);
+  //pesquisar por nome
 
-const searchBar = document.getElementById('searchBar');
+  const searchBar = document.getElementById('searchBar');
 
-console.log(searchBar);
-searchBar.addEventListener('keyup', (e) => {
-  const searchString = e.target.value;
-  const filteredCharacteres = arrayRickAndMorty.filter(item => {
-    return item.name.includes(searchString) ||
-      item.status.includes(searchString) ||
-      item.species.includes(searchString) ||
-      item.gender.includes(searchString)
+  searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    cardsAtuais = cardsAtuais.filter(item => {
+      return item.name.includes(searchString) ||
+        item.status.includes(searchString) ||
+        item.species.includes(searchString) ||
+        item.gender.includes(searchString)
+    });
+    showInfos(cardsAtuais)
   });
-  console.log(filteredCharacteres)
-  showInfos(filteredCharacteres)
-});
 
-var btn = document.querySelector("#voltarAoTopo");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
+
+  var btn = document.querySelector("#voltarAoTopo");
+  btn.addEventListener("click", function() {
+      window.scrollTo(0, 0);
+  });
 
 
 
-
-
-
+  //const calculoStatus = document.getElementById("calculoAgreStatus");
+  //calculoStatus.innerHTML = ${item.status.reduce((acc,curr) => acc+curr} 0)
+  // return acc + curr.
+  // return calculoStatus/cardsAtuais.length
+  //${item.status.reduce((acc,curr) => acc+curr, 0)}</h2><h3> A média de personagens é: ${item.status.reduce((acc,curr) => acc+curr, 0)/cardsAtuais.length}`

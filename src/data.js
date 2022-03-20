504d5afe40ce8fbbaac4bf9c61098eb8fc258ca5
Helena <nannayusuf@gmail.com>
2022-03-20 16:59:54 -0300

    dados

--- src/data.js
+++ src/data.js
@@ -1,11 +1,10 @@
 
-export const searchByName = (name, pokemons) => {
-  return pokemons.filter((select) => select.name.includes(name.toLowerCase()));
-};
+
 
 export const selectType = (selectByType, pokemons) =>
   pokemons.filter((select) => select.type.includes(selectByType));
 
+  
 export const calcType = (pokemons, selectByType) => {
   const type = pokemons.filter(search => search.type.includes(selectByType));
   return Math.round(((type.length * 100) / pokemons.length) * 100) / 100;
@@ -40,3 +39,10 @@ export const sortZA = (pokemon) => {
     return 0;
   });
 }
+
+//busca nome
+export const searchByName = (name, data) => {
+  return data.filter((select) => select.name.includes(name.toLowerCase()));
+ const searchByName = (name, pokemons) => {
+  return pokemons.filter((select) => select.name.includes(name.toLowerCase()));
+}}
--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ function cardsPokemons(data) {
     const cardPokemon = document.getElementById("cards");
     cardPokemon.innerHTML = data.map((item) =>
   `<div class="info-cards"> 
-   <img class="img-card" src="${item.img}"/> 
    <p id="num">  ${item.num}</p> 
+   <img class="img-card" src="${item.img}"/> 
    <p id="name">  Name:${item.name}</p> 
    <p id="type" > Type:${item.type}</p> 
    <p id="about"> About:${item.about}</p> 
@@ -24,19 +24,17 @@ const filterType = document.querySelector(".select-typefilters");
 
 filterType.addEventListener("change", () => {
     const filterbyType = filterType.value;
-    const arrayFiltered = selectType(filterbyType, pokemons);
-    cardsPokemons(arrayFiltered);
+    const arrFilter = selectType(filterbyType, pokemons);
+    cardsPokemons(arrFilter);
     typePercent();
 })
 
 function typePercent() {
     document.getElementById("calculation").innerHTML = "";
-    const filterType = document.getElementById("filter-type").value;
+    const filterType = document.querySelector(".select-typefilters").value;
     let result = calcType(pokemons, filterType);
     document.getElementById("calculation").innerText += `Os pokémons selecionados representam ${result}% do total.`
 }
-console.log(selectType(filterType.value,pokemons))
-
 // Filtro de ordem select
 
 const filterSelectOrder = document.querySelector("#order-search");
@@ -49,12 +47,18 @@ filterSelectOrder.addEventListener("change", (event) => {
 
 // Filtro por input (texto) de nome
 
-const filterInputType = document.querySelector("#search-input");
+const order = document.getElementById('orderby');
+order.addEventListener('change', () => {
+  const orderbyValue = order.value;
+  showData(pokedex.orderByName(data.pokemon, orderbyValue))
+});
 
+const filterInputType = document.querySelector("#inputSeaz");
+console.log(filterInputType)
 filterInputType.addEventListener("change", () => {
     const filterName = filterInputType.value;
-    const arrayFiltered = searchByName(filterName, pokemons);
-    cardsPokemon(arrayFiltered);
+    const arrFilter = searchByName(filterName, pokemons);
+    cardsPokemon(arrFilter);
 })
 
 filterInputType.addEventListener("keyup", (event) => {
@@ -64,7 +68,30 @@ filterInputType.addEventListener("keyup", (event) => {
     }
 
     const filterName = event.target.value;
-    const arrayFiltered = searchByName(filterName, pokemons);
-    cardsPokemons(arrayFiltered);
+    const arrFilter = searchByName(filterName, pokemons);
+    cardsPokemon(arrFilter);
 
 })
+
+
+const backColor = {
+    water: '#6890F0',
+    fire: '#F08030',
+    grass: '#78C850',
+    poison:'#A040A0',
+    bug:'#729f3f',
+    normal:'#a4acaf',
+    flying:'#A890F0',
+    fighting:'#C03028',
+    electric:'#f8d030',
+    ground:'#E0C068',
+    psychic:'#F85888',
+    rock:'#B8A038',
+    ice:'#98D8D8',
+    dragon: '#7038F8',
+    ghost: '#705898',
+    dark:'#705848',
+    steel:'#B8B8D0',
+    fairy:'#B8B8D0',
+    
+    }

2f32fb6948830d72b4ecdb4a73f04954f629ee76
Helena <nannayusuf@gmail.com>
2022-04-01 12:26:10 -0300

    ajustes

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,26 +1,36 @@
-import { selectType, sortAZ} from '../src/data.js';
+import { selectType } from '../src/data.js';
 
+describe('selectType, () => {
+  it('Deve ser uma função', () => {
+    expect(typeof selectType).toBe('function');
+  });
 
- describe('selectType', () => {
-  describe('Verificar a função que filtra por tipo', () => {
-    it('Verificar se é uma função', () => {
-      expect(typeof selectType).toBe('function');
-    });
-    it('Quando usuário colocar o valor, retornar posição da array correta', () => {
-      const typePokemon = 'water';
-      const result = searchType(arrayTest, typePokemon);
-      expect(result).toStrictEqual(typeWater)
-    });
-  })
-})
+  it('Deve retornar pokemons do tipo "bug"', () => {
+    const pokemons = [
+      {"num": "042",
+      "name": "golbat",
+      "type": [
+        "poison",
+        "flying"
+      ]},
 
-  describe('Realizar testes no campo de ordenação', () => {
-    it('Verificar se é uma função', () => {
-      expect(typeof sortAZ).toBe('function');
-    });
-    it('Quando o usuário acionar a função, devolve ordenado por nome (ABC)', () => {
-      expect(sortAZ(arrayTest)).toBe(arrayTest);
-    });
-  })
-});
+      {"num": "001",
+      "name": "bulbasaur",
+      "type": [
+        "grass",
+        "poison"
+      ]},
+
+      {"}num": "010",
+      "name": "caterpie",
+      "type": [
+        "bug"
+      ]}, 
+    ];
+
+    let buscartipo = "bug"
+    const tipo= selectType (buscartipo, pokemons);
+    expect(tipo[0].name).toEqual("caterpie")
  
+  });
+});
